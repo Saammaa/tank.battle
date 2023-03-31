d9bc06c831633e284967362e5120ff4640209c26
wxh <maconi@126.com>
2023-03-31 11:58:50 +0800

    新增节点类。

---
+++ src/cn/edu/ncepu/sa/Model/Element.java
@@ -0,0 +1,66 @@
+package cn.edu.ncepu.sa.Model;
+
+import java.awt.*;
+
+/**
+ * 地图元素类
+ */
+public class Element implements IElement {
+
+    /**
+     * 位置x
+     */
+    public double x;
+
+    /**
+     * 位置 y
+     */
+    public double y;
+
+    public Element() {
+    }
+
+    /**
+     * @param dt
+     */
+    public void update(float dt) {
+
+    }
+
+
+    /**
+     * 在地图上绘制该元素
+     *
+     * @param g
+     */
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+
+    /**
+     * 向某方向移动一段距离
+     *
+     * @param dir 方向
+     * @param len 距离
+     */
+    public void move(double dir, double len) {
+        x = x + len * Math.cos((dir - 90) * Math.PI / 180);
+        y = y + len * Math.sin((dir - 90) * Math.PI / 180);
+
+    }
+
+    /**
+     * 计算两个结点的距离
+     *
+     * @param target 目标元素
+     * @return 距离
+     */
+    public double distance(Element target) {
+        // this,target
+        double a = this.x - target.x;
+        double b = this.y - target.y;
+        return Math.sqrt(a * a + b * b);
+
+    }
+}
---
+++ src/cn/edu/ncepu/sa/Model/IElement.java
@@ -0,0 +1,7 @@
+package cn.edu.ncepu.sa.Model;
+
+import java.awt.*;
+
+public interface IElement {
+    public void draw(Graphics2D g);
+}
